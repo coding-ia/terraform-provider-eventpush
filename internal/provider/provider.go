72e6434bf9d9c54a4ef0b8f675aa66cfe9307d7d
coding-ia <137450628+coding-ia@users.noreply.github.com>
2025-06-11 14:31:47 -0700

    Add region configuration (#3)

--- internal/provider/provider.go
+++ internal/provider/provider.go
@@ -13,17 +13,36 @@ import (
 	"github.com/hashicorp/terraform-plugin-framework/provider"
 	"github.com/hashicorp/terraform-plugin-framework/provider/schema"
 	"github.com/hashicorp/terraform-plugin-framework/resource"
+	"github.com/hashicorp/terraform-plugin-framework/types"
 )
 
 var _ provider.Provider = &EventPushProvider{}
 
 type EventPushProvider struct {
 	version string
+	Meta    Meta
 }
 
 type Meta struct {
+	AWSConfigOptions AWSConfigOptions
+}
+
+type AWSConfigOptions struct {
+	Region string
+}
+
+type AWSClient struct {
 	SQSClient *sqs.Client
 	KMSClient *kms.Client
+	Region    string
+}
+
+type ProviderConfigurationModel struct {
+	AWS *AWSBlockProviderConfigurationModel `tfsdk:"aws"`
+}
+
+type AWSBlockProviderConfigurationModel struct {
+	Region types.String `tfsdk:"region"`
 }
 
 func (e *EventPushProvider) Metadata(ctx context.Context, request provider.MetadataRequest, response *provider.MetadataResponse) {
@@ -34,11 +53,34 @@ func (e *EventPushProvider) Metadata(ctx context.Context, request provider.Metad
 func (e *EventPushProvider) Schema(ctx context.Context, request provider.SchemaRequest, response *provider.SchemaResponse) {
 	response.Schema = schema.Schema{
 		MarkdownDescription: "The Event Push provider contains resource used to send messages to various services.",
+		Blocks: map[string]schema.Block{
+			"aws": schema.SingleNestedBlock{
+				Attributes: map[string]schema.Attribute{
+					"region": schema.StringAttribute{
+						Description: "The region where AWS operations will take place.",
+						Optional:    true,
+					},
+				},
+			},
+		},
 	}
 }
 
 func (e *EventPushProvider) Configure(ctx context.Context, request provider.ConfigureRequest, response *provider.ConfigureResponse) {
+	var config ProviderConfigurationModel
 
+	response.Diagnostics.Append(request.Config.Get(ctx, &config)...)
+
+	if response.Diagnostics.HasError() {
+		return
+	}
+
+	if config.AWS != nil {
+		if !config.AWS.Region.IsNull() {
+			e.Meta.AWSConfigOptions.Region = config.AWS.Region.ValueString()
+		}
+	}
+	response.ResourceData = e.Meta
 }
 
 func (e *EventPushProvider) DataSources(ctx context.Context) []func() datasource.DataSource {
